acba3e1a9090dca8ddd8771c55e1d6bd3f964c23
perssonne <sanchezvaleroe@educ.college-montreal.qc.ca>
2023-08-31 15:37:09 -0400

    Finition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Créé par Enzo Sanchez Valero
 #Créé le 31/08/23
-#TP1 (fonction len)
+#TP1 (fonction split)
 
 print('Ce programme est destiné à compter le nombre de mots d`une phrase de votre choix.')  #Présentation
 nbmots=(len(input('Écrivez votre phrase ici:').split()))    #Définition variable
