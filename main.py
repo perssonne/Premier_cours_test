04fe27c7913e43074215b4c6cacd963e7cd12cbb
sanchezvaleroe <sanchezvaleroe@educ.college-montreal.qc.ca>
2023-09-21 11:10:42 -0400

    MAJ

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Créé par Enzo Sanchez Valero
 #Créé le 31/08/23
-#TP1 (count_word)
+#TP1(count_word)
 
 def count_word(phrase): #Définir la fonction
     resultat=(len(phrase.split(' ')))   #Compter le nombre de mots de la fonction
